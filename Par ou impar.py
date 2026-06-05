2550091ad69cca7c3bcafc450f722485f8cfef38
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-06-05 20:19:20 -0300

    feat: add score tracking and win/loss logic to parity game

--- Par ou impar.py
+++ Par ou impar.py
@@ -1,9 +1,9 @@
 from random import randint
-
+totj = 0
 while True:
-    print("=="*30)
+    print("==" * 30)
     print("JOGO DA ADIVINHAÇÃO")
-    print("=="*30)
+    print("==" * 30)
     n = int(input("Digite um numero: "))
     if n < 0:
         break
@@ -13,20 +13,25 @@ while True:
         print("Valor digitado inválido!")
         pi = input("Impat ou par[i/p] ").upper().strip()[0]
 
-    computador = randint(1,10)
+    computador = randint(1, 10)
 
     total = n + computador
-    
+
     print(f"O jogador escolheu {n} e o computador {computador} no total é {total} ")
-    if total % 2 == 0:
-        resultado = "P"
-        print("È par!")
-    else:
-        resultado = "I"
-        print("È ímpar!")
-    if pi == resultado:
-        print("O JOGADOR GANHOU")
-    else:
-        print("O COMPUTADOR GANHOU!")
+    if pi == "P":
+        if total % 2 == 0:
+            totj += 1
+            print("Você venceu!")
+        else:
+             print("você perdeu!")
+    elif pi == "I":
+        if total % 2 == 1:
+            totj += 1
+            print("você venceu!")
+        else:
+            print("Voce perdeu!")
+            break
+    print("Vamos jogar novamente...")
+print(f"O jogador ganhou {totj} vezes")
     
         
